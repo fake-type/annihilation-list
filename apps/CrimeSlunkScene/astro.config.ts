27a95e1d398e0d8da0ff58395e48ef22d1e33462
Nyarlathotep <anhedonia@skiff.com>
2023-08-23 23:10:56 -0400

    feat: setup astro/Assets

--- apps/CrimeSlunkScene/astro.config.ts
+++ apps/CrimeSlunkScene/astro.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+	experimental: {
+		assets: true,
+	},
 	vite: { plugins: [vanillaExtractPlugin()] },
 })
