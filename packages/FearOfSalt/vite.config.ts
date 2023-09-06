920a530dfdcb30b9b3f8cf5f593b0bad060a46ae
Nyarlathotep <anhedonia@skiff.com>
2023-09-06 03:58:15 -0400

    feat(tests): setup vitest

--- packages/FearOfSalt/vite.config.ts
+++ packages/FearOfSalt/vite.config.ts
@@ -1,10 +1,15 @@
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import reactPlugin from '@vitejs/plugin-react-swc'
 import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	root: 'src',
 	publicDir: resolve(__dirname, 'public'),
+	test: {
+		environment: 'happy-dom',
+		globals: true,
+		dir: __dirname,
+	},
 	plugins: [reactPlugin(), vanillaExtractPlugin()],
 })
